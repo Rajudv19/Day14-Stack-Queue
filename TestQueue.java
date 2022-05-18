35e3f0704ae4799c91a327255875253aab448a79
Rajudv19 <rajudv1997@gmail.com>
2022-05-18 08:52:31 +0530

    Added code for QueueMain

---
+++ TestQueue.java
@@ -0,0 +1,19 @@
+package com.BridgeLabs.day14;
+import java.util.Scanner;
+
+public class TestQueue {
+	public static void main(String[] args) {
+
+		Queue queue = new Queue();
+
+		queue.enQueue(56);
+		queue.enQueue(30);
+		queue.enQueue(70);
+		
+		queue.display();
+
+		for (int i=0;i<4;i++){
+			queue.deQueue();	
+		}
+	}
+}
